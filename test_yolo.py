4bf1f143cb222f9e0d1461a85eecde8ff1c93260
Mario Infang <mario.infang@stud.hslu.ch>
2025-02-23 16:21:40 +0100

    changed model and ajusted skript for pi

--- test_yolo.py
+++ test_yolo.py
@@ -1,23 +1,39 @@
 from detection.path_analyzer import PathAnalyzer
+from picamera import PiCamera
+import time
 import cv2
+import numpy as np
 
 # YOLO-Modell initialisieren
 path_analyzer = PathAnalyzer('detection/model/best.onnx')
 
-# Bild laden (Beispielbild)
-img_path = "../images/T4.jpg"  # Pfad zu deinem Testbild
-img = cv2.imread(img_path)
+# Kamera initialisieren
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.start_preview()
+time.sleep(2)
 
-# Falls Bild nicht geladen werden konnte
-if img is None:
-    print("Fehler: Bild konnte nicht geladen werden.")
-    exit()
+try:
+    while True:
+        # Bild erfassen
+        img = np.empty((480, 640, 3), dtype=np.uint8)
+        camera.capture(img, 'bgr')
 
-# Objekterkennung durchführen
-path_clear = path_analyzer.is_path_clear(img)
+        # Objekterkennung durchführen
+        path_clear = path_analyzer.is_path_clear(img)
 
-# Ergebnisse ausgeben
-if path_clear:
-    print('Weg befahrbar')
-else:
-    print('Weg NICHT befahrbar')
+        # Ergebnisse ausgeben
+        if path_clear:
+            print('Weg befahrbar')
+        else:
+            print('Weg NICHT befahrbar')
+
+        # Optionale Pause (z.B. zur Steuerung der Framerate)
+        time.sleep(0.1)  # Wartezeit in Sekunden
+
+except KeyboardInterrupt:
+    print('Schleife beendet')
+
+finally:
+    camera.stop_preview()
+    camera.close()
