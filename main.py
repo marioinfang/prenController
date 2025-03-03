6519fc7bdf5f7213a46c994a706535b8e5069bc8
taasane1 <nevilan.sabanathan@swisscom.com>
2025-03-03 16:01:10 +0100

    set proper port

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from communication.uart_handler import UARTHandler
 from state_machine.state_machine import CarStateMachine
 
 if __name__ == "__main__":
-    uart = UARTHandler("some/port", 115200, 2)
+    uart = UARTHandler("/dev/serial0", 115200, 2)
     fsm = CarStateMachine(uart)
     fsm.run()
