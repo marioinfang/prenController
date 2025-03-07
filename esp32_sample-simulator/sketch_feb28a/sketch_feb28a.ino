7c93644ded5b0496f9c18b05d1575805122c589f
taasane1 <nevilan.sabanathan@swisscom.com>
2025-03-07 07:25:28 +0100

    add more commands

--- esp32_sample-simulator/sketch_feb28a/sketch_feb28a.ino
+++ esp32_sample-simulator/sketch_feb28a/sketch_feb28a.ino
@@ -9,7 +9,7 @@ HardwareSerial uart(2);
 void setup() {
     Serial.begin(BAUD_RATE);
     uart.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
-    Serial.println("ESP32 UART gestartet");
+    Serial.println("ESP32 initialized");
 }
 
 void loop() {
@@ -17,13 +17,32 @@ void loop() {
         String command = uart.readStringUntil('\n');
         command.trim();
 
-        Serial.print("Empfangen: ");
+        Serial.print("Received: ");
         Serial.println(command);
 
-        if (command == "WEG_BEFAHREN") {
+        if (command == "INIT_COMPLETE") {
             delay(random(500, 4000));
-            uart.println("WEGPUNKT_ERREICHT");
-        } 
+            uart.println("ack");
+        }
+        else if (command == "FAHRE_WEITER") {
+            bool randomizedNumb = random(0, 1);
+            if (randomizedNumb) {
+                uart.println("WEGPUNKT_ERREICHT");
+            } else {
+                uart.println("ack");
+            }
+        }
+        else if (command == "WEG_BEFAHREN") {
+            delay(random(500, 4000));
+             bool randomizedNumb = random(0, 2);
+            if (randomizedNumb == 0) {
+                uart.println("AUSRICHTUNG_NOTWENDIG");
+            } else if(randomizedNumb == 1){
+                uart.println("ZIEL_ERKANNT");
+            } else {
+                uart.println("ack");
+            }
+        }
         else if (command == "WEGPUNKT_ANALYSIEREN") {
             delay(random(500, 4000));
             uart.println("ZIEL_ERKANNT");
@@ -36,7 +55,15 @@ void loop() {
             uart.println("HEBEN");
             delay(random(500, 4000));
             uart.println("HEBEN_FERTIG");
-        } 
+        }
+        else if (command == "IDENTIFIZIERE_ZIEL"){
+            bool randomizedNumb = random(0, 1);
+            if (randomizedNumb) {
+                uart.println("HINDERNIS_GEFUNDEN");
+            } else {
+                uart.println("ack");
+            }
+        }
         else {
             uart.println("ERROR");
         }
